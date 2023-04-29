e49b316711b75e5806e0f35b385e240f6cb836ed
skywalk <61213518+skywalk1411@users.noreply.github.com>
2023-04-29 10:19:42 -0400

    Create key.js

---
+++ config/key.js
@@ -0,0 +1,4 @@
+const discordkey = 'type_discord_token_here';
+const channelId = 'type_discord_channelId_to_spam_here';
+const alertId = 'type_discord_channelId_to_alert_here';
+module.exports = { discordkey : discordkey, channelId : channelId, alertId : alertId };
